22642fed9ec94d0ef168920b66b03ea6c32fff50
ysknk <31568245+ysknk@users.noreply.github.com>
2023-01-06 13:12:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,25 +20,43 @@ glob.sync(argv.src, {
 }).map((key) => {
   let filepath = key
 
-  if (argv.dest) {
-    const ext = argv.ext || key.match(/\.[^/.]+$/)[0]
-    const filename = key.replace(/\.[^/.]+$/, '')
-    filepath = path.resolve(argv.dest, `${filename}${ext}`)
-  }
+  ;(async () => {
+    if (argv.dest) {
+      const ext = argv.ext || key.match(/\.[^/.]+$/)[0]
+      const filename = key.replace(/\.[^/.]+$/, '')
+      filepath = path.resolve(argv.dest, `${filename}${ext}`)
+      try {
+        try {
+          // TODO: Unexpected file after build
+          const webpFilepath = filepath.replace(/\.(.*)$/, '.webp')
+          const isAccess = await utils.file.stat(webpFilepath)
+          filepath = webpFilepath
+        } catch (e) {
+          const isAccess = await utils.file.stat(filepath)
+        }
+      } catch (e) {
+        // console.log(e)
+      }
+    }
 
-  fs.readFile(filepath, (err, data) => {
-    // if (err) { throw err }
-    if (err) { return }
+    fs.readFile(filepath, (err, data) => {
+      // if (err) { throw err }
+      if (err) { return }
 
-    (async () => {
-      const files = await deleteAsync(filepath, {
-        force: argv.force,
-        dryRun: argv.dryRun
-      })
+      ;(async () => {
+        try {
+          const files = await deleteAsync(filepath, {
+            force: argv.force,
+            dryRun: argv.dryRun
+          })
 
-      const message = `deleted${argv.dryRun ? '[dryrun]' : ''}: `
-      utils.message.success(message + files.join('\n' + message), {ptime: false})
-    })()
-  })
+          const message = `deleted${argv.dryRun ? '[dryrun]' : ''}: `
+          utils.message.success(message + files.join('\n' + message), {ptime: false})
+        } catch (e) {
+          console.log(e)
+        }
+      })()
+    })
+  })()
 })
 
